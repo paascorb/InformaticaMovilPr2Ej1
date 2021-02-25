99a40c56fbd60ff6341492d4195d77c12c97ce42
Pablo Ascorbe Fernández <paascorb@unirioja.loc>
2021-02-25 18:40:09 +0100

    Commit inicial

---
+++ app/src/main/java/com/example/twoacivities/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.twoacivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void lanzarSegundaActividad(View view) {
+        Log.d(LOG_TAG, "¡Botón clicado!");
+
+        Intent
+    }
+}
